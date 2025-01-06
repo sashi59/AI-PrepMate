c004401c4916afd328c5db0a592b0eb3c2ba20e9
sashi59 <ranjudevi66291@gmail.com>
2025-01-07 03:00:38 +0530

    Get started page updated

--- app/dashboard/_components/InterviewList.jsx
+++ app/dashboard/_components/InterviewList.jsx
@@ -8,33 +8,45 @@ import InterviewCard from "./InterviewCard";
 
 const InterviewList = () => {
   const [interviewList, setInterviewList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { user } = useUser();
   useEffect(() => {
     getAllInterviews();
     // eslint-disable-next-line
   }, [user]);
   const getAllInterviews = async () => {
+    setLoading(true);
     const result = await db
       .select()
       .from(MockInterview)
       .where(
         eq(MockInterview?.createdBy, user?.primaryEmailAddress?.emailAddress)
-      ).orderBy(desc(MockInterview.id));
+      )
+      .orderBy(desc(MockInterview.id));
 
-    console.log("result", result);
+    // console.log("result", result);
     setInterviewList(result);
+    setLoading(false);
   };
   return (
     <div>
       <h1 className="text-3xl my-10 font-bold">Your Previous AI Interview</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {interviewList &&
-          interviewList.length > 0 &&
-          interviewList.map((interviewList, idx) => 
-            <InterviewCard key={interviewList?.mockId || idx} idx={idx} interviewList={interviewList}/>
-          )}
-      </div>
+      {loading ? (
+        <div className="text-center text-3xl font-bold">Loading...</div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {interviewList &&
+            interviewList.length > 0 &&
+            interviewList.map((interviewList, idx) => (
+              <InterviewCard
+                key={interviewList?.mockId || idx}
+                idx={idx}
+                interviewList={interviewList}
+              />
+            ))}
+        </div>
+      )}
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>Hello</h1>
-      <Button>Click me</Button>
+    <div className="flex items-center justify-center   min-h-screen ">
+
+      <div>
+        <h1 className="text-3xl md:text-5xl font-bold text-black mb-1">AI PrepMate</h1>
+        <p className="text-lg text-gray-600 ">Learn AI with confidence</p>
+      
+      <Button className="w-full my-5" onClick={()=> router.replace("/dashboard")} >Get Started</Button>
+      </div>
     </div>
   );
 }
